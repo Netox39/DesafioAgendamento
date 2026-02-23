aed52e5f848c6c1fc460b1b33d37005f6b98fca8
Manoel Neto <152178564+Netox39@users.noreply.github.com>
2026-02-23 12:29:46 -0300

    Update CorsConfig.java

--- backend/src/main/java/com/projetounichristus/backend/config/CorsConfig.java
+++ backend/src/main/java/com/projetounichristus/backend/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.DesafioAgendamento.config;
+package com.projetounichristus.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
