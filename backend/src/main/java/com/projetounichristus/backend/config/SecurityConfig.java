8fde2c7941913ce9bcafaebe71604a4f4a0d9679
Manoel Neto <152178564+Netox39@users.noreply.github.com>
2026-02-23 14:56:27 -0300

    Update SecurityConfig.java

--- backend/src/main/java/com/projetounichristus/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/projetounichristus/backend/config/SecurityConfig.java
@@ -1,16 +1,11 @@
 package com.projetounichristus.backend.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -27,5 +22,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-   
+}
