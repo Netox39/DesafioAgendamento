a7d5cb21e6845ffb6400988e89125901d02e4168
Manoel Neto <152178564+Netox39@users.noreply.github.com>
2026-02-23 15:46:58 -0300

    Update CorsConfig.java

--- backend/src/main/java/com/projetounichristus/backend/config/CorsConfig.java
+++ backend/src/main/java/com/projetounichristus/backend/config/CorsConfig.java
@@ -1,42 +1,4 @@
-package com.projetounichristus.backend.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.List;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(List.of(
-            "https://desafio-agendamento-one.vercel.app"
-        ));
-
-        config.setAllowedMethods(List.of(
-                "GET",
-                "POST",
-                "PUT",
-                "DELETE",
-                "OPTIONS"
-        ));
-
-        config.setAllowedHeaders(List.of("*"));
-
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**", config);
-
-        return source;
-    }
-}
+config.setAllowedOrigins(List.of("https://desafio-agendamento-one.vercel.app"));
+config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+config.setAllowedHeaders(List.of("*"));
+config.setAllowCredentials(true);
